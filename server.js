167b96866aa80c2545c2273011d0e535ee62180e
IJJUROUTHU HEMANTH <hemanthijjurouthu@gmail.com>
2025-03-12 21:42:02 +0530

    Added authentication, routing, and database updates

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
 const express = require('express');
 const connectDB = require('./config/database');
-const User = require('./Models/User');
+
 const app = express();
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
-app.post('/signup',async (req,res) => {
-    const user = new User({
-        firstName : "Ramakrishna",
-        lastName : "Ijjurouthu",
-        emailId : "Ramakrishna@gmail.com",
-        password : "Ram@1234#",
-        age : "35",
-        gender : "Male",
-    });
+app.use(cors({
+    origin : "http://localhost:5173",
+    credentials : true
+}));
 
-    await user.save();
-    res.send("user saved successfully!!!");
-})
+app.use(cookieParser());
+app.use(express.json());
+
+const authRouter = require('./Routers/auth');
+const profileRouter = require('./Routers/profile');
+const requestRouter = require('./Routers/request');
+const userRouter = require('./Routers/user');
+
+app.use("/",authRouter);
+app.use("/",profileRouter);
+app.use("/",requestRouter);
+app.use("/",userRouter);
 
 connectDB()
 .then(() => {
