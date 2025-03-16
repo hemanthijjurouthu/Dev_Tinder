d22309c4a2375361e27a00b2c6b144f7135edd70
IJJUROUTHU HEMANTH <hemanthijjurouthu@gmail.com>
2025-03-16 22:58:19 +0530

    added

--- Routers/initializeSocket.js
+++ Routers/initializeSocket.js
@@ -3,7 +3,6 @@ const Chat = require('../Models/chat');
 
 const initializeSocket = (server) => {
     const io = new Server(server, {
-        path: "/api/socket-io",
         cors : {
             origin : 'http://localhost:5173',
         },
