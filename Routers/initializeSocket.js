ac11ea1c81c553321f776bc0a0c07fc0d5dbff5a
IJJUROUTHU HEMANTH <hemanthijjurouthu@gmail.com>
2025-03-16 23:21:55 +0530

    added

--- Routers/initializeSocket.js
+++ Routers/initializeSocket.js
@@ -4,7 +4,9 @@ const Chat = require('../Models/chat');
 const initializeSocket = (server) => {
     const io = new Server(server, {
         cors : {
-            origin : 'http://localhost:5173',
+            origin : ['http://localhost:5173',
+                "https://ijjurouthu.duckdns.org"
+            ]
         },
     });
 
